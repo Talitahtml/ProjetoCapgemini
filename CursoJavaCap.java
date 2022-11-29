a523d13561cced0d03696a22629e74ee86a269af
Talitahtml <97572310+Talitahtml@users.noreply.github.com>
2022-11-29 18:09:30 -0300

    Add files via upload

---
+++ CursoJavaCap.java
@@ -0,0 +1,26 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package curso.java.cap;
+
+/**
+ *
+ * @author elica
+ */
+public class CursoJavaCap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String
+        nome = "Maria";
+        int idade = 32;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2flreais%n", nome, idade, renda);
+    }
+    
+}
